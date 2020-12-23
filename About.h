9da9b32a15b9cf4b80014445cad2bb0f7fb65d5a
930913 <3722064+930913@users.noreply.github.com>
2020-12-23 19:59:37 +0000

    Add compile info to About app

--- About.h
+++ About.h
@@ -1,10 +1,18 @@
 class About : public App {
+  private:
+    static uint8_t conv2d(const char* p) {
+      uint8_t v = 0;
+      if ('0' <= *p && *p <= '9')
+        v = *p - '0';
+      return 10 * v + *++p - '0';
+    }
+    uint8_t hh=conv2d(__TIME__), mm=conv2d(__TIME__+3), ss=conv2d(__TIME__+6);  // Get H, M, S from compile time
   public:
     char* getMenuName() override {
       return "About";
     }
 
-    void onSetup(TFT_eSPI tft) override {
+    void onSetup(TFT_eSPI& tft) override {
       Util::Screen::fillScreen(tft, TFT_BLACK);
       tft.setTextColor(TFT_WHITE, TFT_BLACK);
       tft.setTextSize(1);
@@ -13,6 +21,7 @@ class About : public App {
 
       tft.println("About");
       tft.println("This multi-application system was made using Framework32");
+      tft.printf("Compiled:\n - On: %s\n - At: %02d:%02d:%02d", __DATE__, hh, mm, ss);
     };
 };
 
