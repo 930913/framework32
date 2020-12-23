c4685f94387d2d53cd4dd283294ea94e218eb1bc
930913 <3722064+930913@users.noreply.github.com>
2020-12-23 19:59:58 +0000

    Remove config printing
    Probably best to not print all those secrets.

--- configuration.h
+++ configuration.h
@@ -4,7 +4,7 @@ StaticJsonDocument<4096> configDoc;
 
 void initialiseConfig() {
   Serial.println("Loading config");
-  Serial.println(configJson);
+  //Serial.println(configJson);
   DeserializationError err = deserializeJson(configDoc, configJson);
   if (err) {
     Serial.print(F("deserializeJson() failed with code "));
