7d850aa5cbf04f9f188d75cc6c17cdeef0ccdb75
930913 <3722064+930913@users.noreply.github.com>
2020-12-25 14:16:20 +0000

    Correct y grid names
    Left -> Top, Right -> Bottom

--- util.h
+++ util.h
@@ -56,16 +56,16 @@ namespace Util {
     namespace Grid {
       // x/y = orientation
       // 1/2/3/4 = how many blocks of the screen make up this grid
-      // l/m/r = coordinate for left/middle/right
+      // l/t / m / r/b = coordinate for left/top / middle / right/bottom
       // 1 .. blocks = which number block in grid
       // E.g. y3m2() will give you the y coordinate for the middle of the second block in a grid of three
       // E.g. x2r1() will give you the y coordinate for the right of the first block in a grid of two
 
       int calculate(int len, int pieces, char alignment, int pieceNum) {
         int block = (len / pieces);
-        if(alignment == 'l') return block * (pieceNum - 1);
+        if(alignment == 'l' || alignment == 't') return block * (pieceNum - 1);
         if(alignment == 'm') return block * (pieceNum - 1) + block / 2;
-        if(alignment == 'r') return block * pieceNum;
+        if(alignment == 'r' || alignment == 'b') return block * pieceNum;
       }
 
       int x1l1() { return calculate(getWidth(), 1, 'l', 1); }
@@ -102,39 +102,39 @@ namespace Util {
       int x4m4() { return calculate(getWidth(), 4, 'm', 4); }
       int x4r4() { return calculate(getWidth(), 4, 'r', 4); }
 
-      int y1l1() { return calculate(getHeight() - 10, 1, 'l', 1) + 10; }
+      int y1t1() { return calculate(getHeight() - 10, 1, 't', 1) + 10; }
       int y1m1() { return calculate(getHeight() - 10, 1, 'm', 1) + 10; }
-      int y1r1() { return calculate(getHeight() - 10, 1, 'r', 1) + 10; }
+      int y1b1() { return calculate(getHeight() - 10, 1, 'b', 1) + 10; }
 
-      int y2l1() { return calculate(getHeight() - 10, 2, 'l', 1) + 10; }
+      int y2t1() { return calculate(getHeight() - 10, 2, 't', 1) + 10; }
       int y2m1() { return calculate(getHeight() - 10, 2, 'm', 1) + 10; }
-      int y2r1() { return calculate(getHeight() - 10, 2, 'r', 1) + 10; }
-      int y2l2() { return calculate(getHeight() - 10, 2, 'l', 2) + 10; }
+      int y2b1() { return calculate(getHeight() - 10, 2, 'b', 1) + 10; }
+      int y2t2() { return calculate(getHeight() - 10, 2, 't', 2) + 10; }
       int y2m2() { return calculate(getHeight() - 10, 2, 'm', 2) + 10; }
-      int y2r2() { return calculate(getHeight() - 10, 2, 'r', 2) + 10; }
+      int y2b2() { return calculate(getHeight() - 10, 2, 'b', 2) + 10; }
 
-      int y3l1() { return calculate(getHeight() - 10, 3, 'l', 1) + 10; }
+      int y3t1() { return calculate(getHeight() - 10, 3, 't', 1) + 10; }
       int y3m1() { return calculate(getHeight() - 10, 3, 'm', 1) + 10; }
-      int y3r1() { return calculate(getHeight() - 10, 3, 'r', 1) + 10; }
-      int y3l2() { return calculate(getHeight() - 10, 3, 'l', 2) + 10; }
+      int y3b1() { return calculate(getHeight() - 10, 3, 'b', 1) + 10; }
+      int y3t2() { return calculate(getHeight() - 10, 3, 't', 2) + 10; }
       int y3m2() { return calculate(getHeight() - 10, 3, 'm', 2) + 10; }
-      int y3r2() { return calculate(getHeight() - 10, 3, 'r', 2) + 10; }
-      int y3l3() { return calculate(getHeight() - 10, 3, 'l', 3) + 10; }
+      int y3b2() { return calculate(getHeight() - 10, 3, 'b', 2) + 10; }
+      int y3t3() { return calculate(getHeight() - 10, 3, 't', 3) + 10; }
       int y3m3() { return calculate(getHeight() - 10, 3, 'm', 3) + 10; }
-      int y3r3() { return calculate(getHeight() - 10, 3, 'r', 3) + 10; }
+      int y3b3() { return calculate(getHeight() - 10, 3, 'b', 3) + 10; }
 
-      int y4l1() { return calculate(getHeight() - 10, 4, 'l', 1) + 10; }
+      int y4t1() { return calculate(getHeight() - 10, 4, 't', 1) + 10; }
       int y4m1() { return calculate(getHeight() - 10, 4, 'm', 1) + 10; }
-      int y4r1() { return calculate(getHeight() - 10, 4, 'r', 1) + 10; }
-      int y4l2() { return calculate(getHeight() - 10, 4, 'l', 2) + 10; }
+      int y4b1() { return calculate(getHeight() - 10, 4, 'b', 1) + 10; }
+      int y4t2() { return calculate(getHeight() - 10, 4, 't', 2) + 10; }
       int y4m2() { return calculate(getHeight() - 10, 4, 'm', 2) + 10; }
-      int y4r2() { return calculate(getHeight() - 10, 4, 'r', 2) + 10; }
-      int y4l3() { return calculate(getHeight() - 10, 4, 'l', 3) + 10; }
+      int y4b2() { return calculate(getHeight() - 10, 4, 'b', 2) + 10; }
+      int y4t3() { return calculate(getHeight() - 10, 4, 't', 3) + 10; }
       int y4m3() { return calculate(getHeight() - 10, 4, 'm', 3) + 10; }
-      int y4r3() { return calculate(getHeight() - 10, 4, 'r', 3) + 10; }
-      int y4l4() { return calculate(getHeight() - 10, 4, 'l', 4) + 10; }
+      int y4b3() { return calculate(getHeight() - 10, 4, 'b', 3) + 10; }
+      int y4t4() { return calculate(getHeight() - 10, 4, 't', 4) + 10; }
       int y4m4() { return calculate(getHeight() - 10, 4, 'm', 4) + 10; }
-      int y4r4() { return calculate(getHeight() - 10, 4, 'r', 4) + 10; }
+      int y4b4() { return calculate(getHeight() - 10, 4, 'b', 4) + 10; }
     }
   }
 
